9dc3dd94b6cb4e6226a2692c54c3413f2e61a4b0
rishnan <johan.trino.1998@gmail.com>
2026-03-06 16:16:02 +0100

    Initial commit

---
+++ Eventorias/EventoriasApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventoriasApp.swift
+//  Eventorias
+//
+//  Created by Johan Trino on 06/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct EventoriasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
